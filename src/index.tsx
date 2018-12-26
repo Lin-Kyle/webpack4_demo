16637ea0a66bac12a37ed40d81ca0c687a1d216a
linyukai <linyukai@mizhua.com>
2018-12-26 17:53:04 +0800

    first

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import {
+  HashRouter
+} from "react-router-dom";
+import Main from "PAGE/main";
+import "../index.html";
+
+ReactDOM.render(
+  <HashRouter>
+    <Main />
+  </HashRouter>,
+  document.getElementById("root")
+);
