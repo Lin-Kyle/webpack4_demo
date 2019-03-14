66d77f014930237f01ba1f94fa767dee64955652
linyukai <linyukai@mizhua.com>
2019-03-14 19:52:58 +0800

    updated

--- src/page/main.js
+++ src/page/main.js
@@ -1,9 +1,13 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import { Switch, Route, Redirect, Link } from "react-router-dom";
 import { hot } from "react-hot-loader";
-import View1 from "CMT/view1";
-import View2 from "CMT/view2";
+import View1 from "CMT/view1.jsx";
+import View2 from "CMT/view2.jsx";
 import "STYLE/style.scss";
+import { Layout, Menu } from 'antd';
+
+const { Header, Content, Footer } = Layout;
+
 class Main extends Component {
   constructor(props, context) {
     super(props, context);
@@ -14,18 +18,34 @@ class Main extends Component {
 
   render() {
     return (
-      <Fragment>
-        <p>{this.state.title}</p>
-        <Link to="/view1/">View1</Link>
-        <Link to="/view2/">View2</Link>
-        <Switch>
-          <Route exact path="/" component={View1} />
-          <Route path="/view1/" component={View1} />
-          <Route path="/view2/" component={View2} />
-          <Redirect to="/" />
-        </Switch>
-      </Fragment>
-    );
+      <Layout className="layout">
+        <Header>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1"><Link to="/view1/">View1</Link></Menu.Item>
+            <Menu.Item key="2"><Link to="/view2/">View2</Link></Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ padding: '0 50px' }}>
+          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+            <h2>{this.state.title}</h2>
+            <Switch>
+              <Route exact path="/" component={View1} />
+              <Route path="/view1/" component={View1} />
+              <Route path="/view2/" component={View2} />
+              <Redirect to="/" />
+            </Switch>
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    )
   }
 }
 
